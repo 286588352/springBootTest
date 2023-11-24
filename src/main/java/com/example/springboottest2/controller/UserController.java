65e0ebc15e38cfe4aaf4d678a40a87dbd08f35e1
s286588352 <286588352@qq.com>
2023-11-24 12:19:21 +0800

    已从主干显示与本分支差异获取文件，然后上传推送

--- src/main/java/com/example/springboottest2/controller/UserController.java
+++ src/main/java/com/example/springboottest2/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.example.springboottest2.controller;
 
-import com.example.springboottest2.entity.User;
-import com.example.springboottest2.response.Result;
-import com.example.springboottest2.service.UserService;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Objects;
+import com.example.springboottest2.entity.User;
+import com.example.springboottest2.response.Result;
+import com.example.springboottest2.service.UserService;
 
 /**
  * @Author SS
- * @Date 2021/1/23 9:37
+ * @Date 2021/1/23
  * @Version 1.0
  */
 @RestController
@@ -25,7 +26,9 @@ public class UserController {
         List<User> list =userService.query();
         if(Objects.nonNull(list)){
             return Result.OK(list);
-        }else return Result.OK("无数据");
+        } else {
+            return Result.OK("无数据");
+        }
     }
 
 }
