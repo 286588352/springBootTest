14955840fae28bb20a9d113a2b2fc6f5930ceb4b
s286588352 <286588352@qq.com>
2023-11-24 11:07:48 +0800

    已从主干显示与本分支差异获取文件，然后上传推送

---
+++ src/main/java/com/example/springboottest2/controller/testController.java
@@ -0,0 +1,15 @@
+package com.example.springboottest2.controller;
+
+/**
+ * @BelongsProject: springBootTest
+ * @BelongsPackage: com.example.springboottest2.controller
+ * @Author: 邵帅
+ * @CreateTime: 2023-11-24
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class testController {
+    /**
+     * 提交到主干之后再合到其他分支测试可行性
+     */
+}
